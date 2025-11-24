5d6b1120c3e0e7ba843ff25240bfd203c88fe6dd
noman <alnomaan30@gmail.com>
2025-11-24 12:06:57 +0600

    first commit

---
+++ src/component/QuizStart.jsx
@@ -0,0 +1,22 @@
+import { BookOpen } from 'lucide-react'
+import React from 'react'
+
+const QuizStart = () => {
+  return (
+    <div className='max-w-4xl mx-auto'>
+        <div className='bg-white rounded-2xl shadow-xl p-8 text-center'>
+            <div className='mb-8'>
+                <div className='inline-flex items-center justify-center w-24 h-24 bg-gradient-to-br from-blue-100 to-purple-100 rounded-full mb-6'>
+                    <BookOpen className='w-12 h-12'/>
+
+                </div>
+                
+            </div>
+
+        </div>
+      
+    </div>
+  )
+}
+
+export default QuizStart
